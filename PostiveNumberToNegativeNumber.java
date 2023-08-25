42de498974949d68495bd7158a30d050689b74ed
SowmiyaJayabal <97733485+SowmiyaJayabal@users.noreply.github.com>
2023-08-25 20:13:15 +0400

    Add files via upload

---
+++ PostiveNumberToNegativeNumber.java
@@ -0,0 +1,17 @@
+package week1Assignments;
+
+public class PostiveNumberToNegativeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=-50;
+		if(num1<0) {
+			int num2=-1*num1;
+			System.out.println("The Postitive number for"+num1+" is " +num2);
+		}
+		
+	
+
+	}
+
+}
